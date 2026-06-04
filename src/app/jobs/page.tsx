08fe8df85748102252d165bcbb130ed3b021ae55
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-06-04 13:13:55 +0200

    new

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,7 +1,7 @@
 import { DashboardLayout } from "@/components/layout/Layout";
 import  JobsClient  from "@/features/jobs/components/JobsClient"
 
-export default async function JobsPage() {
+export default function JobsPage() {
 
     return (
       <DashboardLayout>
