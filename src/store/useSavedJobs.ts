491c155b051f429cef66860363edcee0fe7ff11a
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-06-04 16:21:51 +0200

    new fix

--- src/store/useSavedJobs.ts
+++ src/store/useSavedJobs.ts
@@ -1,45 +1,46 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { Job } from "@/features/jobs/types/job"
+import { persist, createJSONStorage } from "zustand/middleware";
+import { Job } from "@/features/jobs/types/job";
 
 interface SavedJobsStore {
-    savedJobs: Job[]
-    addJob: (job: Job) => void
-    removeJob: (id: number) => void
-    isSaved: (id: number) => boolean
+  savedJobs: Job[];
+  addJob: (job: Job) => void;
+  removeJob: (id: number) => void;
+  isSaved: (id: number) => boolean;
 }
 
-
 export const useSavedJobs = create<SavedJobsStore>()(
-    persist(
-        (set, get) => ({
-            savedJobs: [],
+  persist(
+    (set, get) => ({
+      savedJobs: [],
 
-            addJob: (job) =>
-                set((state) => {
-                    const alreadySaved = state.savedJobs.some(
-                        (savedJobs) => savedJobs.id === job.id
-                    )
-                    if(alreadySaved){
-                        return state
-                    }
-                    return{
-                        savedJobs: [...state.savedJobs, job]
-                    }
-                    
-                }),
-            
-            removeJob: (id) =>
-                set((state) => ({
-                    savedJobs: state.savedJobs.filter(
-                        (job) => job.id !== id
-                    )
-                })),
-            
-                isSaved: (id) =>
-                    get().savedJobs.some((job) => job.id === id)
+      addJob: (job) =>
+        set((state) => {
+          const alreadySaved = state.savedJobs.some(
+            (savedJob) => savedJob.id === job.id
+          );
 
+          if (alreadySaved) return state;
+
+          return {
+            savedJobs: [...state.savedJobs, job],
+          };
         }),
-        {name: "saved-jobs"},
-    )
-)
+
+      removeJob: (id) =>
+        set((state) => ({
+          savedJobs: state.savedJobs.filter((job) => job.id !== id),
+        })),
+
+      isSaved: (id) =>
+        get().savedJobs.some((job) => job.id === id),
+    }),
+    {
+      name: "saved-jobs",
+
+      storage: createJSONStorage(() => localStorage),
+
+      skipHydration: true,
+    }
+  )
+);
