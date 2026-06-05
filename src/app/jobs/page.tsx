21962372d425563238cb372ca2757899bed544fa
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-06-05 09:46:51 +0200

    fix jobs page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,19 +1,16 @@
 import { DashboardLayout } from "@/components/layout/Layout";
-import  JobsClient  from "@/features/jobs/components/JobsClient"
-
+import JobsClient from "@/features/jobs/components/JobsClient";
+import { Suspense } from "react";
 
 export default async function JobsPage() {
-
-    return (
-      <DashboardLayout>
-        <div className="space-y-6">
-          <h1 className="text-3xl font-bold">
-            Jobs
-          </h1>
-
-            <JobsClient />
-        </div>
-      </DashboardLayout>
-    );
-  
+  return (
+    <DashboardLayout>
+      <div className="space-y-6">
+        <h1 className="text-3xl font-bold">Jobs</h1>
+        <Suspense fallback={<div>Loading jobs...</div>}>
+          <JobsClient />
+        </Suspense>{" "}
+      </div>
+    </DashboardLayout>
+  );
 }
